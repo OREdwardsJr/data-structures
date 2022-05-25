3662a79bacd647779970b261f559256dca80d1f6
OREdwardsJr <97556216+OREdwardsJr@users.noreply.github.com>
2022-05-25 01:44:13 -0700

    updated

---
+++ Leetcode/Dynamic Programming/198 House Robber.py
@@ -0,0 +1,40 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        prev_1, prev_2 = 0, 0
+        
+        for num in nums:
+            prev_1, prev_2 = prev_2, max(prev_1 + num, prev_2)
+            
+        return prev_2
+
+
+'''
+MEDIUM 
+
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+
+Example 2:
+
+Input: nums = [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+Total amount you can rob = 2 + 9 + 1 = 12.
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 100
+    0 <= nums[i] <= 400
+'''        
