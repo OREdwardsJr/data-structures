a5a544312ea17dc7ed547065a6a9e9ff72b8359f
OREdwardsJr <97556216+OREdwardsJr@users.noreply.github.com>
2022-05-28 17:56:41 -0700

    Updated

--- Heaps/min_heap.py
+++ Heaps/min_heap.py
@@ -1,6 +1,8 @@
 """
 Min Heap Implementation in Python
 """
+
+
 class MinHeap:
     def __init__(self):
         """
@@ -8,7 +10,7 @@ class MinHeap:
         """
         self.heap_list = [0]
         self.current_size = 0
- 
+
     def sift_up(self, i):
         """
         Moves the value up in the tree to maintain the heap property.
@@ -17,10 +19,13 @@ class MinHeap:
         while i // 2 > 0:
             # If the element is less than its parent swap the elements
             if self.heap_list[i] < self.heap_list[i // 2]:
-                self.heap_list[i], self.heap_list[i // 2] = self.heap_list[i // 2], self.heap_list[i]
+                self.heap_list[i], self.heap_list[i // 2] = (
+                    self.heap_list[i // 2],
+                    self.heap_list[i],
+                )
             # Move the index to the parent to keep the properties
             i = i // 2
- 
+
     def insert(self, k):
         """
         Inserts a value into the heap
@@ -31,7 +36,7 @@ class MinHeap:
         self.current_size += 1
         # Move the element to its position from bottom to the top
         self.sift_up(self.current_size)
- 
+
     def sift_down(self, i):
         # if the current node has at least one child
         while (i * 2) <= self.current_size:
@@ -39,43 +44,48 @@ class MinHeap:
             mc = self.min_child(i)
             # Swap the values of the current element is greater than its min child
             if self.heap_list[i] > self.heap_list[mc]:
-                self.heap_list[i], self.heap_list[mc] = self.heap_list[mc], self.heap_list[i]
+                self.heap_list[i], self.heap_list[mc] = (
+                    self.heap_list[mc],
+                    self.heap_list[i],
+                )
             i = mc
- 
+
     def min_child(self, i):
         # If the current node has only one child, return the index of the unique child
-        if (i * 2)+1 > self.current_size:
+        if (i * 2) + 1 > self.current_size:
             return i * 2
         else:
             # Herein the current node has two children
             # Return the index of the min child according to their values
-            if self.heap_list[i*2] < self.heap_list[(i*2)+1]:
+            if self.heap_list[i * 2] < self.heap_list[(i * 2) + 1]:
                 return i * 2
             else:
                 return (i * 2) + 1
- 
+
     def delete_min(self):
         # Equal to 1 since the heap list was initialized with a value
         if len(self.heap_list) == 1:
-            return 'Empty heap'
- 
+            return "Empty heap"
+
         # Get root of the heap (The min value of the heap)
         root = self.heap_list[1]
- 
+
         # Move the last value of the heap to the root
         self.heap_list[1] = self.heap_list[self.current_size]
- 
+
         # Pop the last value since a copy was set on the root
-        *self.heap_list, _ = self.heap_list
- 
+        self.heap_list.pop()
+
         # Decrease the size of the heap
         self.current_size -= 1
- 
+
         # Move down the root (value at index 1) to keep the heap property
         self.sift_down(1)
- 
+
         # Return the min value of the heap
         return root
+
+
 """
 Driver program
 """
@@ -89,6 +99,4 @@ my_heap.insert(13)
 my_heap.insert(11)
 my_heap.insert(10)
 
-print(my_heap.delete_min()) # removing min node i.e 5 
-
- 
+print(my_heap.delete_min())  # removing min node i.e 5
