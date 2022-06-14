2e37b7116628c91347fcfe72d444d8e0d6d2d6fa
OREdwardsJr <97556216+OREdwardsJr@users.noreply.github.com>
2022-06-14 02:52:12 -0700

    Final Version

--- merge-sort.py
+++ merge-sort.py
@@ -1,85 +1,51 @@
-#Iterative approach
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return
 
-a = [2, 8, 15, 18]
-b = [5, 9, 12, 17]
+    mid = len(arr)//2
 
-def merge_fxn(list1: list, list2: list) -> list:
-    if list1 == [] and list2 == []:
-        return []
-    elif list1 == []:
-        return list2
-    elif list2 == []:
-        return list1
-    else:
-        output = []
-        while list1 != [] and list2 != []:
-            if list1[0] < list2[0]:
-                output.append(list1.pop(0))
-                if list1 == []:
-                    return output + list2
-            elif list2[0] < list1[0]:
-                output.append(list2.pop(0))
-                if list2 == []:
-                    return output + list1
-            else:
-                output.append(list1.pop(0))
-                list2.pop(0)
+    left = arr[:mid]
+    right = arr[mid:]
 
-print(merge_fxn(a, b))   
+    merge_sort(left)
+    merge_sort(right)
 
+    merge_two_sorted_lists(left, right, arr)
 
+def merge_two_sorted_lists(a,b,arr):
+    len_a = len(a)
+    len_b = len(b)
 
-#Recursive Approach
-a = [2, 8, 15, 18, 25, 27, 39]
-b = [5, 9, 12, 17]
+    i = j = k = 0
 
-def merge_fxn(list1: list, list2: list, output=[]) -> list:
-    if list1 == [] and list2 == []:
-        return output
-    elif list1 == []:
-        return output + list2
-    elif list2 == []:
-        return output + list1
-    else:
-        if list1[0] < list2[0]:
-            output.append(list1.pop(0))
-        elif list2[0] < list1[0]:
-            output.append(list2.pop(0))
+    while i < len_a and j < len_b:
+        if a[i] <= b[j]:
+            arr[k] = a[i]
+            i+=1
         else:
-            output.append(list1.pop(0))
-            list2.pop(0)
-        return merge_fxn(list1, list2, output)
+            arr[k] = b[j]
+            j+=1
+        k+=1
 
-print(merge_fxn(a, b))   
+    while i < len_a:
+        arr[k] = a[i]
+        i+=1
+        k+=1
 
+    while j < len_b:
+        arr[k] = b[j]
+        j+=1
+        k+=1
 
-#Merging more than 2 lists
-'''
-You need to find a way to capture when a list is empty. 
-You tried indexing the empty list but and empty list isn't subscriptable. 
-'''
+if __name__ == '__main__':
+    test_cases = [
+        [10, 3, 15, 7, 8, 23, 98, 29],
+        [],
+        [3],
+        [9,8,7,2],
+        [1,2,3,4,5]
+    ]
 
-a = [4, 6, 12, 10**4]
-b = [3, 5, 9, 10**4]
-c = [1, 10, 16, 10**4]
-d = [2, 4, 8, 10**4]
-
-def merge_multiple(list1: list, list2: list, list3: list, list4: list, output=[]) -> list:
-    if len(list1 + list2 + list3 + list4) == 4:
-        return output
-    else:
-        min_val = min(list1[0], list2[0], list3[0], list4[0])
-        if (list1[0] == min_val):
-            output.append(list1.pop(0))
-        elif (list2[0] == min_val):
-            output.append(list2.pop(0))
-        elif (list3[0] == min_val):
-            output.append(list3.pop(0))
-        elif (list4[0] == min_val):
-            output.append(list4.pop(0))
-        else: 
-            output.append(list4.pop(0))
-        
-        return merge_multiple(list1, list2, list3, list4, output)
-
-print(merge_multiple(a, b, c, d))
+    for arr in test_cases:
+        merge_sort(arr)
+        print(arr)
